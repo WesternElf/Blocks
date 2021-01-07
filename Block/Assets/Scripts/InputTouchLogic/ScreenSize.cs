d222bf2c2ccba574f11aa8e52c51fdab95011389
Daria <dashaost17@gmail.com>
2021-01-07 19:58:39 +0200

    project created

---
+++ Block/Assets/Scripts/InputTouchLogic/ScreenSize.cs
@@ -0,0 +1,15 @@
+﻿namespace  InputTouchLogic
+{
+    public class ScreenSize
+    {
+        public float Height { get; set; } 
+        public float Width { get; set; }
+
+        public ScreenSize(float height, float width)
+        {
+            Height = height;
+            Width = width;
+        }
+    }
+}
+
---
+++ Block/Assets/Scripts/Interfaces/IInitiable.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Interfaces
+{
+    public interface IInitiable<T1>
+    {
+        void Init(T1 obj1);
+    }
+
+    public interface IInitiable<T1, T2>
+    {
+        void Init(T1 obj1, T2 obj2);
+    }
+
+    public interface IInitiable<T1, T2, T3>
+    {
+        void Init(T1 obj1, T2 obj2, T3 obj3);
+    }
+
+    public interface IInitiable<T1, T2, T3, T4>
+    {
+        void Init(T1 obj1, T2 obj2, T3 obj3, T4 obj4);
+    }
+
+    public interface IInitiable<T1, T2, T3, T4, T5>
+    {
+        void Init(T1 obj1, T2 obj2, T3 obj3, T4 obj4, T5 obj5);
+    }
+
+    public interface IInitiable<T1, T2, T3, T4, T5, T6> {
+        void Init(T1 obj1,T2 obj2,T3 obj3,T4 obj4,T5 obj5,T6 obj6);
+    }
+}
+
